957812a79239ddcd5f18a351cd2a40f82ab65a51
jze9898 <016101476d@uandina.edu.pe>
2021-03-17 10:07:06 -0500

    Ex. Picker del formulario

--- components/products/AddProductForm.js
+++ components/products/AddProductForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Button, Input } from 'react-native-elements'
 import CountryPicker from 'react-native-country-picker-modal'
+import {Picker} from '@react-native-picker/picker'
 
 export default function AddProductForm({ toastRef, setLoading, navigation }) {
     const addProduct = () => {
@@ -24,6 +25,10 @@ function FormAdd(){
     const [country, setCountry] = useState("PE")
     const [callingCode, setCallingCode] = useState("51")
     const [phone, setPhone] = useState("")
+    const [selectedClass, setSelectedClass] = useState("")
+    const [selectedType, setSelectedType] = useState("")
+    const [selectedFont, setSelectedFont] = useState("")
+    const [selectedAttentionType, setSelectedAttentionType] = useState("")
 
     return (
         <View style={styles.viewForm}>
@@ -35,20 +40,64 @@ function FormAdd(){
                 placeholder="Nombre del restaurante."
                 
             />
-            <Input
+            <Picker
+                selectedValue={selectedClass}
+                style={styles.pickerClass}
+                onValueChange={(itemValue) =>
+                   setSelectedClass(itemValue) }
+            >
+                <Picker.Item label="Comida" value="comida"/>
+                <Picker.Item label="Bebida" value="bebida"/>
+                
+            </Picker>
+            {/* <Input
                 placeholder="Clasificacion."
                 
-            />
-            <Input
+            /> */}
+            <Picker
+                selectedValue={selectedType}
+                style={styles.pickerClass}
+                onValueChange={(itemValue) =>
+                   setSelectedType(itemValue) }
+            >
+                <Picker.Item label="Casero" value="casero"/>
+                <Picker.Item label="Restaurante" value="restaurante"/>
+                <Picker.Item label="Quinta" value="quinta"/>
+                <Picker.Item label="Tipica" value="tipica"/>
+                <Picker.Item label="Extravagante" value="extravagante"/>
+                <Picker.Item label="Vegana" value="vegana"/>
+                
+            </Picker>
+            {/* <Input
                 placeholder="Tipo."
                 
-            />
-            <Input
+            /> */}
+            <Picker
+                selectedValue={selectedFont}
+                style={styles.pickerClass}
+                onValueChange={(itemValue) =>
+                   setSelectedFont(itemValue) }
+            >
+                <Picker.Item label="Mariscos" value="mariscos"/>
+                <Picker.Item label="Pescado" value="pescado"/>
+                <Picker.Item label="Carnes" value="carnes"/>
+                <Picker.Item label="Pollo" value="pollo"/>
+                <Picker.Item label="Cerdo" value="cerdo"/>
+                <Picker.Item label="Vegetales" value="vegetales"/>
+                <Picker.Item label="Pavo" value="pavo"/>
+                <Picker.Item label="Bebidas calientes" value="bebidacaliente"/>
+                <Picker.Item label="Bebidas frias" value="bebidasfrias"/>
+                <Picker.Item label="Bebidas alcoholicas" value="bebidasalcoholicas"/>
+                <Picker.Item label="Empanadas" value="empanadas"/>
+                <Picker.Item label="Postres" value="postres"/>
+            </Picker>
+            {/* <Input
                 placeholder="Fuente."
                 
-            />
+            /> */}
             <Input
                 placeholder="Direccion del restaurante."
+                multiline
                 
             />
             <View style={styles.phoneView}>
@@ -70,10 +119,20 @@ function FormAdd(){
                     containerStyle={styles.inputPhone}
                 />
             </View>
-            <Input
+            <Picker
+                selectedValue={selectedAttentionType}
+                style={styles.pickerClass}
+                onValueChange={(itemValue) =>
+                   setSelectedAttentionType(itemValue) }
+            >
+                <Picker.Item label="Delivery" value="delivery"/>
+                <Picker.Item label="Consumo en local" value="consumolocal"/>
+                <Picker.Item label="Recojo en local" value="recojolocal"/>
+            </Picker>
+            {/* <Input
                 placeholder="Tipo de atencion."
                 
-            />
+            /> */}
             <Input
                 placeholder="Descripcion del producto."
                 multiline
@@ -91,7 +150,8 @@ function FormAdd(){
 
 const styles = StyleSheet.create({
     viewContainer: {
-        height: "100%"
+        height: "100%",
+        marginTop: 6
     },
     viewForm: {
         marginHorizontal: 10
@@ -111,5 +171,11 @@ const styles = StyleSheet.create({
     btnAddProduct: {
         margin: 20,
         backgroundColor: "#721c1c"
+    },
+    pickerClass: {
+        width: "100%",
+        height: 35,
+        marginBottom: 10,
+        marginLeft: 2
     }
 })
