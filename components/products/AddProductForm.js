84dbf5d415d1b8d0bc469178a2c18a68e76cb779
jze9898 <016101476d@uandina.edu.pe>
2021-03-17 12:55:59 -0500

    67. Eliminar imagen del formulario

--- components/products/AddProductForm.js
+++ components/products/AddProductForm.js
@@ -81,6 +81,7 @@ function UploadImage({ toastRef, imagesSelected, setImagesSelected }) {
             ],
             {
                 cancelable: true
+                //canelable
             }
         )
     }
