44f00a634412e19eeafb0f407ff09a9763b9d4f2
jze9898 <016101476d@uandina.edu.pe>
2021-03-23 10:10:29 -0500

    82. Obteniendo informacion del Producto

--- screens/products/Product.js
+++ screens/products/Product.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Alert, StyleSheet, Text, View } from 'react-native'
+
+import Loading from '../../components/Loading'
+import { getDocumentById } from '../../utils/actions'
 
 export default function Product({ navigation, route }) {
     const { id, nameProduct } = route.params
+    const [product, setProduct] = useState(null)
     
     navigation.setOptions({ title: nameProduct })
+
+    useEffect(() => {
+        (async() => {
+            const response = await getDocumentById("products", id)
+            if (response.statusResponse) {
+                setProduct(response.document)
+            } else {
+                setProduct({})
+                Alert.alert("Ocurrio un problema cargando la opcion escogida.")
+            }
+        })()
+    }, [])
+
+    if (!product){
+        return <Loading isVisible={true} text="Cargando..."/>
+    }
     return (
         <View>
-            <Text>Product...</Text>
+            <Text>{product.description}</Text>
         </View>
     )
 }
